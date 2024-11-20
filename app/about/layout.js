24e98bb3fe7ce3f69e50c417ea61c863bcf8f0c9
aminul-islam-sajid <85724932+aminul-islam-sajid@users.noreply.github.com>
2024-11-20 22:42:35 +0600

    error handaling

--- app/about/layout.js
+++ app/about/layout.js
@@ -5,8 +5,8 @@ const AboutLAyout = ({ children }) => {
     return (
         <div>
             <ul className="flex gap-6">
-                <li><Link href={'about/misson'}>Mission</Link></li>
-                <li><Link href={'about/vision'}>Vision</Link></li>
+                <li><Link href={'/about/misson'}>Mission</Link></li>
+                <li><Link href={'/about/vision'}>Vision</Link></li>
             </ul>
             {children}
         </div>
---
+++ app/about/loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const loading = () => {
+    return (
+        <div>
+            loading About segment
+        </div>
+    );
+};
+
+export default loading;
