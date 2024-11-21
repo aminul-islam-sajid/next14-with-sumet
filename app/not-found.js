fda29c5371954b6d58c993a58237f6df4198ab11
aminul-islam-sajid <85724932+aminul-islam-sajid@users.noreply.github.com>
2024-11-21 06:55:44 +0600

    notFound page add

---
+++ app/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const notFound = () => {
+    return (
+        <div>
+            page is not found !!!!!!!!!!!!
+        </div>
+    );
+};
+
+export default notFound;
