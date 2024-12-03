c7a5a5ecb5c5fabdc224cad8c471a9d7fc8282e7
aminul-islam-sajid <85724932+aminul-islam-sajid@users.noreply.github.com>
2024-12-03 18:47:11 +0600

    img quliti set , 1-100 100 is better quality

--- app/about/misson/page.js
+++ app/about/misson/page.js
@@ -8,7 +8,7 @@ const page = () => {
             misson page
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam asperiores, nam expedita quod ratione, magni, earum saepe consequuntur adipisci perferendis quo impedit qui. Quam quo iste ullam omnis enim tempora.</p>
             <Button />
-            <Image src={img} alt='group picture' />
+            <Image src={img} alt='group picture' quality={75} />
         </div>
     );
 };
