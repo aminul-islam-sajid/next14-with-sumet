58c61a101b71002d352219f42d5ac50734b425b6
aminul-islam-sajid <85724932+aminul-islam-sajid@users.noreply.github.com>
2024-12-03 18:41:13 +0600

    img add

--- app/about/misson/page.js
+++ app/about/misson/page.js
@@ -1,4 +1,5 @@
-
+import Image from 'next/image';
+import img from "@/public/images/thumb.jpg"
 import Button from '@/app/components/Button';
 import React from 'react';
 const page = () => {
@@ -7,6 +8,7 @@ const page = () => {
             misson page
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam asperiores, nam expedita quod ratione, magni, earum saepe consequuntur adipisci perferendis quo impedit qui. Quam quo iste ullam omnis enim tempora.</p>
             <Button />
+            <Image src={img} alt='group picture' />
         </div>
     );
 };
