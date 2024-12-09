244d00a07648cc8290a536a83a369b6c16b4c85b
aminul-islam-sajid <85724932+aminul-islam-sajid@users.noreply.github.com>
2024-12-09 18:04:44 +0600

    post page create

--- app/posts/page.js
+++ app/posts/page.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function page() {
+const page = () => {
     return (
         <div>
-
+            <h1 className='my-8 text-xl font-semibold'>posts page</h1>
         </div>
     );
-}
+};
 
 export default page;
