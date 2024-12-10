25d4dc06e955b8aab3f7a80a814d873f9c0d2079
aminul-islam-sajid <85724932+aminul-islam-sajid@users.noreply.github.com>
2024-12-10 12:32:32 +0600

    dynamic post show

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,10 +1,13 @@
+import getPost from '@/app/lib/getPost';
 import React from 'react';
 
-const postPage = (params) => {
+const postPage = async ({ params }) => {
     const { id } = params;
+    const post = await getPost(id)
     return (
-        <div>
-            <h2></h2>
+        <div className='m-2'>
+            <h2 className='my-2'>{post.title}</h2>
+            <p>{post.body}</p>
         </div>
     );
 };
