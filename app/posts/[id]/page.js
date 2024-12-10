d96edca30c21c2398449a0c4db655adb53beb02a
aminul-islam-sajid <85724932+aminul-islam-sajid@users.noreply.github.com>
2024-12-10 12:24:33 +0600

    post page, singel post get

---
+++ app/posts/[id]/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const postPage = (params) => {
+    const { id } = params;
+    return (
+        <div>
+            <h2></h2>
+        </div>
+    );
+};
+
+export default postPage;
