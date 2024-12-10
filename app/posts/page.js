689ea7039c45c36508ebe0d41d3988e8378770d9
aminul-islam-sajid <85724932+aminul-islam-sajid@users.noreply.github.com>
2024-12-10 12:14:55 +0600

    posts show

--- app/posts/page.js
+++ app/posts/page.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import getAllPosts from '../lib/getAllPosts';
+import Link from 'next/link';
 
-const page = () => {
+const page = async () => {
+    const posts = await getAllPosts()
     return (
         <div>
             <h1 className='my-8 text-xl font-semibold'>posts page</h1>
+            <ul className='mt-6'>
+                {
+                    posts.map(post => <li key={post.id}>
+                        <Link href={`/posts/${post.id}`}>{post.title}</Link>
+                    </li>)
+                }
+            </ul>
         </div>
     );
 };
